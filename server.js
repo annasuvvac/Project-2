1076311d221f7bd466fa7b420ccd859afdb60c78
Brittany <britloy91@gmail.com>
2020-02-03 18:49:56 -0500

    "hotel api key and installed axios"

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+const axios = require("axios");
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
@@ -18,8 +18,31 @@ app.engine('handlebars', expbs({defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //routing 
+
+
 app.get('/', (req, res) => {
-    res.render('index');
+  axios({
+    "method":"GET",
+    "url":"https://hotels4.p.rapidapi.com/locations/search",
+    "headers":{
+    "content-type":"application/json",
+    "x-rapidapi-host":"hotels4.p.rapidapi.com",
+    "x-rapidapi-key":"0678ac7d4dmsh7ec92c295c5eef2p194aa9jsnf39296021020"
+    },"params":{
+    "locale":"en_US",
+    "query":"new york"
+    }
+    })
+    .then((response)=>{
+      console.log(response.data)
+      res.render('index');
+
+    })
+    .catch((error)=>{
+      res.render('index');
+
+      console.log(error)
+    })
 });
 
 
@@ -28,5 +51,12 @@ app.listen(PORT, () => {
 
 });
 
+
+
+
+
+
+
+  
 
           
