a84f6ce8efefe685c9d818f6141cba18f820b408
Ana Suvac <anasuvac@yahoo.com>
2020-02-06 21:00:16 -0500

    fixed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
@@ -17,8 +18,32 @@ app.engine('handlebars', expbs({defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //routing 
+
+
 app.get('/', (req, res) => {
-    res.render('index');
+  axios({
+    "method":"GET",
+    "url":"https://hotels4.p.rapidapi.com/locations/search",
+    "headers":{
+    "content-type":"application/json",
+    "x-rapidapi-host":"hotels4.p.rapidapi.com",
+    "x-rapidapi-key":"0678ac7d4dmsh7ec92c295c5eef2p194aa9jsnf39296021020"
+    },
+    "params":{
+    "locale":"en_US",
+    "query":"new york"
+    }
+    })
+    .then((response)=>{
+      console.log(response.data)
+      res.render('index');
+
+    })
+    .catch((error)=>{
+      res.render('index');
+
+      console.log(error)
+    })
 });
 
 
