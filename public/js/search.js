4074af2c41140e0f1f8627585d91fb17566af56e
Brittany <britloy91@gmail.com>
2020-02-08 11:30:17 -0500

    search and enter are now working

--- public/js/search.js
+++ public/js/search.js
@@ -1,49 +1,87 @@
+/* eslint-disable indent */
 function searchCity(userInput) {
-    var queryUrl = "https://hotels4.p.rapidapi.com/locations/search?query=" + userInput ;
+    var queryUrl = 'https://hotels4.p.rapidapi.com/locations/search?query=' + userInput;
     $.ajax({
         url: queryUrl,
-        method: "GET",
+        method: 'GET',
         'headers': {
-            'content-type':'application/json',
-            'x-rapidapi-host':'hotels4.p.rapidapi.com',
-            'x-rapidapi-key':'0678ac7d4dmsh7ec92c295c5eef2p194aa9jsnf39296021020'
-        },'params':{
-            'locale':'en_US',
+            'content-type': 'application/json',
+            'x-rapidapi-host': 'hotels4.p.rapidapi.com',
+            'x-rapidapi-key': '0678ac7d4dmsh7ec92c295c5eef2p194aa9jsnf39296021020'
+        }, 'params': {
+            'locale': 'en_US',
         }
     }).then(function (response) {
-        console.log(response);
-        // console.log(response.hotel[0].hotel.events_url);
-    
-        // for (var i = 0; i < response.hotel.length; i++) {
-        //     var box = $('<div class="boxes" id="box-' + i + '">');
-        //     var right = $('<div class="pies" id="pie-' + i + '">');
-        //     var name = $("<h4>").addClass("Name").text(response.hotel[i].hotel.name);
-        //     var phone = $("<div>").addClass("Phone").text('Phone Number: ' + response.hotel[i].hotel.phone_numbers);
-        //     var address = $("<div>").addClass("Locations").text('Address: ' + response.hotels[i].hotel.location.address);
-    
-        //     console.log(barURL);
+        // console.log(response);
+        console.log(response.suggestions[3].entities);
 
-           
-        //     right.append(name, phone, hotelURL, ratings, address);
-        //     box.append(img);
-           
-        //     box.append(right);
-        //     $("#displayBox").append(box)
-            
+        var resultArray = response.suggestions[3].entities;
+
+        // for (var i = 0; i < resultArray.length; i++) {
+        //   console.log(resultArray[i].name);
         // }
+
+        for (var i = 0; i < resultArray.length; i++) {
+
+            var box = $('<div class="card-panel" id="box-' + i + '">');
+            var outerBox = $('<div class="col s12 m4">');
+
+            var icon = $('<i class="material-icons large teal-text">').text('store');
+            var name = $('<h4>').addClass('Name').text(resultArray[i].name);
+
+            var caption = $('<p>').addClass('captions').html(resultArray[i].caption);
+
+
+            box.append(icon);
+            box.append(name);
+            box.append(caption);
+            outerBox.append(box);
+
+            $('#hotel-results').append(outerBox);
+
+
+
+        }
     });
 };
 
-$("#searchCityButton").on("click", function (event) {
-  
+$('#searchCityButton').on('click', function (event) {
+
+
     event.preventDefault();
-  
-    var userInput = $("#userInput").val().trim();
+
+    var userInput = $('#userInput').val().trim();
     console.log(userInput);
-  
+
     searchCity(userInput);
 });
 
-function clear() {
-    $("#displayBox").empty();
-}
+// function clear() {
+//   $('#displayBox').empty();
+// }
+
+$('#userInput').submit(function () {
+    event.preventDefault();
+
+    var userInput = $('#userInput').val().trim();
+    console.log(userInput);
+
+    searchCity(userInput);
+
+});
+
+$(document).ready(function () {
+    $(window).keydown(function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+
+            var userInput = $('#userInput').val().trim();
+            console.log(userInput);
+
+            searchCity(userInput);
+
+        }
+    });
+});
+
+
